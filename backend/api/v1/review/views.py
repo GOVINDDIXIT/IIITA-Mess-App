2fa9f265c0741f768caa1125a37035e769816217
dojutsu-user <vaibhgupt199@gmail.com>
2018-08-22 20:30:22 +0530

    [Backend] Add Check To Restrict Only One Form Submission/Day From App

--- backend/api/v1/review/views.py
+++ backend/api/v1/review/views.py
@@ -1,18 +1,26 @@
 from django.conf import settings
+from django.utils import timezone
 
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from . import serializers
+from review.models import ReviewQuestion
 
 
 @api_view(['POST'])
 def answer_review_questions(request):
     """View To Submit Ques-Ans For Mess Review"""
 
-    access_token = request.META.get('access_token')
+    access_token = request.META.get('HTTP_TOKEN')
     if access_token == settings.MESS_API_ACCESS_TOKEN:
+        if (ReviewQuestion.objects.all().count() > 0):
+            last_post_date = ReviewQuestion.objects.all().order_by(
+                '-timestamp').first().timestamp.date()
+            today_date = timezone.now().date()
+            if (last_post_date == today_date):
+                return Response({'detail': 'Data Already Submitted'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         data = request.data.get('questions')
         serializer = serializers.ReviewQuestionSerializer(data=data, many=True)
 

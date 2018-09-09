b6929f31e6b148e8ebac427c848308ee5bead368
dojutsu-user <vaibhgupt199@gmail.com>
2018-09-09 23:15:27 +0530

    Add docstrings and comments.

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -11,19 +11,41 @@ from review.models import ReviewQuestion
 
 
 def homepage(request):
+    """
+    View to get all the reports and show them on
+    on the homepage of the blog.
+    """
+    # Flag variable to check whether any reports exits or not.
     status = True
     all_objects = ReviewQuestion.objects.all()
+
     if not all_objects.exists():
+        # Assigning 'status' to False since there are no reports exist.
         status = False
+
+    # set of all dates for which mess reports are present.
     all_dates_set = {object_.timestamp.date() for object_ in all_objects}
+
+    # list of all dates for which mess reports are present.
     all_dates_list = list(all_dates_set)
     all_dates_list.sort(reverse=True)
+
     reports = []
     for date in all_dates_list:
-        queryset = ReviewQuestion.objects.filter(timestamp__date=date, ques_type='DAILY')
+        queryset = ReviewQuestion.objects.filter(
+            timestamp__date=date, ques_type='DAILY')
+
+        # Appending the queryset for the given date in the 'reports' list.
+        # This is done to support pagination and also it makes the rendering
+        # on the frontend less cumbersome.
         reports.append(queryset)
+
+    # Getting the first page if not passed explicitly.
     page = request.GET.get('page', 1)
+
+    # Posts limit per page.
     paginator = Paginator(reports, 20)
+
     try:
         posts = paginator.page(page)
     except PageNotAnInteger:
@@ -35,12 +57,19 @@ def homepage(request):
 
 
 def view_post(request, date):
+    """View to report for a given date"""
+
     report = ReviewQuestion.objects.filter(timestamp__date=date)
+
+    # Flag variable to check if report for the
+    # given date exists or not.
     status = report.exists()
     return render(request, 'blog/view_post.html', {'report': report, 'status': status, 'date': date})
 
 
 def previous_reports(request):
+    """Searching the report of a given date"""
+
     if request.method == 'POST':
         date = request.POST.get('date')
         queryset = ReviewQuestion.objects.filter(timestamp__date=date)
@@ -49,11 +78,17 @@ def previous_reports(request):
 
 
 def developers(request):
+    """Developer's Page"""
+
     return render(request, 'blog/developers.html', {})
 
 
 @csrf_exempt
 def today_report_submitted(request):
+    """
+    View to check if the report for that day has been
+    submitted or not
+    """
     queryset = ReviewQuestion.objects.filter(
         timestamp__date=timezone.now().date())
     if not queryset.exists():

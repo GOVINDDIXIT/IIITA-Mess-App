bc2342355c407fbf056e8d101180cfd44cc54c18
Govind Dixit <32304546+GOVINDDIXIT@users.noreply.github.com>
2019-01-01 19:00:08 +0530

    removed unused imports

--- app/src/main/java/com/iiita/messmanagement/CustomAdapter.java
+++ app/src/main/java/com/iiita/messmanagement/CustomAdapter.java
@@ -2,9 +2,7 @@ package com.iiita.messmanagement;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
